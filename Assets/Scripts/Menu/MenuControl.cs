163429c26c55ddf012fcba3947f8ed37d3463f75
OlavDeng2 <olavkruithof@gmail.com>
2018-01-27 15:09:36 +0100

    added main menu as well as basic level manager.

---
+++ Assets/Scripts/Menu/MenuControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(Canvas))]
+public class MenuControl : MonoBehaviour
+{
+    private MenuPanel[] menuPanels;
+
+    void Awake()
+    {
+        // Find all menu panels (including disabled ones)
+        menuPanels = GetComponentsInChildren<MenuPanel>(true);
+
+        ShowFirstPanel();
+    }
+
+    public void ShowFirstPanel()
+    {
+        // Disable all panels
+        foreach (MenuPanel menuPanel in menuPanels)
+        {
+            menuPanel.gameObject.SetActive(false);
+        }
+
+        // Activate the first one
+        menuPanels[0].gameObject.SetActive(true);
+    }
+
+    public void ShowPanel(MenuPanel menuPanel)
+    {
+        // Loop trough all panels
+        foreach (MenuPanel _menuPanel in menuPanels)
+        {
+            // Set it active it equals the given panel
+            _menuPanel.gameObject.SetActive(menuPanel.Equals(_menuPanel));
+        }
+    }
+
+    public void ShowPopupPanel(MenuPanel menuPanel)
+    {
+        menuPanel.gameObject.SetActive(true);
+    }
+
+    public void HidePanel(MenuPanel menuPanel)
+    {
+        menuPanel.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Menu/MenuUtil.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MenuUtil : MonoBehaviour {
+    public LevelManager levelManager = new LevelManager();
+
+    public void LoadLevel(string levelName)
+    {
+        levelManager.LoadScene(levelName);
+    }
+
+
+    public void Exit() {
+		#if UNITY_EDITOR
+			Debug.Break();
+		#else
+			Application.Quit();
+		#endif
+    }
+}
